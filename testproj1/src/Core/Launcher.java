985e4366861402cba80aec3361c0772cef71b319
Lukas <lukas.eckert@gmx.de>
2012-10-05 20:38:06 +0200

    first
    Signed-off-by: Lukas <lukas.eckert@gmx.de>

---
+++ testproj1/src/Core/Launcher.java
@@ -0,0 +1,14 @@
+package Core;
+
+public class Launcher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	     if (true) {
+	    	 System.out.println("sdfhsjdhfk");
+	     }
+	}
+
+}
